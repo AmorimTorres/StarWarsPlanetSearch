0e777cfecc9b79c5ac9c792180f1ef2a79d14556
André <amorimtorresandre@gmail.com>
2022-03-22 15:01:45 -0300

    REQ01: fetching API on useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Provider from './context/myProvider';
 
 function App() {
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const data = await response.json();
+      const dataResults = data.results;
+      console.log(dataResults);
+      // setPeople(data);
+    };
+    fetchAPI();
+  }, []);
+
   return (
     <Provider>
       <span>Hello, App!!!! Lets Rock!</span>
